16e97605aacc6e6c3edab5d995c102cbb7fa2298
ashleyhull-versent <ashley.hull@versent.com.au>
2018-08-20 00:43:14 +1000

    Update New_Instance_Part_4.sh

--- New_Instance_Part_4.sh
+++ New_Instance_Part_4.sh
@@ -2,7 +2,9 @@ docker-compose down
 docker-compose build
 docker-compose run --rm web rails assets:precompile
 docker-compose run --rm web rails db:migrate
+
+sudo chown -R 991:991 /home/mastodon/mastodon/public/system
+
 docker-compose up -d
 sudo systemctl restart nginx.service
 
-sudo chown -R 991:991 /home/mastodon/mastodon/public/system
