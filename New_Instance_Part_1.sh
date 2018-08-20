e5241e6fb456ca81ed058c2dbd0c95d3aed90e9e
ashleyhull-versent <ashley.hull@versent.com.au>
2018-08-20 19:18:40 +1000

    fixed swap after reboot

--- New_Instance_Part_1.sh
+++ New_Instance_Part_1.sh
@@ -35,6 +35,6 @@ sudo fallocate -l 5G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 ##############################################################
 
